51cb9f20c8461554aa1c9062dc8c1b9c63752c74
jtkaufman737 <jtkaufman737@gmail.com>
2022-01-01 14:30:57 -0500

    First commit of final hello faunadb example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App(props) {
+  const [pokemon, setPokemon] = useState([]);
+  const q = props.q
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const response = await props.db.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection("pokemon"))),
+          q.Lambda(item => q.Get(item))
+        )
+      )
+      
+      let updatedPokemon = response.data.map(item => {
+          return item.data
+      })
+
+      setPokemon(updatedPokemon)
+    }
+
+    fetchPokemon()
+  }, [])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home">
+        <h3>Pokemon</h3>
+        <section className="grid">
+          {
+            pokemon.map(p => {
+              return (
+                <div key={p.id}>
+                  <h6>{p.name}</h6>
+                  <img src={p.imageUrl} alt={p.name} className="pokemon"/>
+                </div>
+              )
+            })
+          }
+        </section>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faunadb from 'faunadb';
+
+const db = new faunadb.Client({
+  secret: process.env.REACT_APP_FAUNADB_SECRET, 
+  domain: 'db.us.fauna.com',
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App 
+      db={db}
+      q={faunadb.query} 
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
